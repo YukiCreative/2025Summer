4723091d86f9f7625a6494de2e0891b8f3e22748
YukiCreative <YukiNakahiraCreative@gmail.com>
2025-06-23 18:52:27 +0900

    Update PlayerLockOnMoveBack.cpp

--- 2025Summer/Actor/Player/PlayerLockOnMoveBack.cpp
+++ 2025Summer/Actor/Player/PlayerLockOnMoveBack.cpp
@@ -1,8 +1,20 @@
 #include "PlayerLockOnMoveBack.h"
+#include "PlayerLockOnMoveFoward.h"
+#include "PlayerLockOnMoveRight.h"
+#include "PlayerLockOnMoveLeft.h"
+#include "PlayerLockOnIdle.h"
+#include "Player.h"
+#include "AnimationModel.h"
+
+namespace
+{
+	const std::string kAnimName = "Armature|WalkBack";
+}
 
 PlayerLockOnMoveBack::PlayerLockOnMoveBack(std::weak_ptr<Player> parent) :
 	PlayerState(parent)
 {
+	m_player.lock()->m_model->ChangeAnimation(kAnimName);
 }
 
 PlayerLockOnMoveBack::~PlayerLockOnMoveBack()
