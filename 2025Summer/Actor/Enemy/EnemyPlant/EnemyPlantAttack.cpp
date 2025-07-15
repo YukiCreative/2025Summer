021ba25eb80b8f31775d81ae59da67908475a480
YukiCreative <YukiNakahiraCreative@gmail.com>
2025-07-15 14:41:50 +0900

    up

--- 2025Summer/Actor/Enemy/EnemyPlant/EnemyPlantAttack.cpp
+++ 2025Summer/Actor/Enemy/EnemyPlant/EnemyPlantAttack.cpp
@@ -1,5 +1,10 @@
 #include "EnemyPlantAttack.h"
 
+namespace
+{
+	const std::string kAnimName = "";
+}
+
 EnemyPlantAttack::EnemyPlantAttack(std::weak_ptr<EnemyPlant> parent) :
 	EnemyPlantState(parent)
 {
