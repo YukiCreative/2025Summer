ef488a63072887b31e1ef87955abd7d61ced4709
YukiCreative <YukiNakahiraCreative@gmail.com>
2025-08-14 23:57:29 +0900

    up

--- 2025Summer/Actor/Player/PlayerDodge.cpp
+++ 2025Summer/Actor/Player/PlayerDodge.cpp
@@ -1,7 +1,8 @@
 #include "PlayerDodge.h"
 
 PlayerDodge::PlayerDodge(std::weak_ptr<Player> parent) :
-	PlayerState(parent)
+	PlayerState(parent),
+	m_frame(0)
 {
 }
 
@@ -11,5 +12,11 @@ PlayerDodge::~PlayerDodge()
 
 std::shared_ptr<PlayerState> PlayerDodge::Update()
 {
+	// モーションを流す
+	// 特定フレームで無敵
+	// 特定フレームの間に攻撃を受けるとジャスト回避
+
+	++m_frame;
+
 	return shared_from_this();
 }
